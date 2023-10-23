f08bb6eba4d09f441a79c46adb03f39749326908
daver987 <daveyalexander@outlook.com>
2023-10-23 15:12:35 -0400

    Add Seo meta tags

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,6 +4,33 @@ definePageMeta({
   path: '/contact',
   colorMode: 'dark',
 })
+
+useSeoMeta({
+  title: 'Contact Elite Business Connections - Get in Touch',
+  description: 'Contact Elite Business Connections to learn more about business networking opportunities and how you can join our community.',
+  ogTitle: 'Contact Elite Business Connections - Get in Touch',
+  ogDescription: 'Contact Elite Business Connections to learn more about business networking opportunities and how you can join our community.',
+  ogImage: 'https://elitebusinessconnections.ca//images/ebc_logo_light.png',
+  ogUrl: 'https://elitebusinessconnections.ca/contact/',
+  twitterTitle: 'Contact Elite Business Connections - Get in Touch',
+  twitterDescription: 'Contact Elite Business Connections to learn more about business networking opportunities and how you can join our community.',
+  twitterImage: 'https://elitebusinessconnections.ca//images/ebc_logo_light.png',
+  twitterCard: 'summary'
+})
+
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
 </script>
 
 <template>
--- pages/spotlights/[slug].vue
+++ pages/spotlights/[slug].vue
@@ -6,6 +6,33 @@ definePageMeta({
   colorMode: 'dark',
 })
 
+useSeoMeta({
+  title: 'Member Spotlights - Elite Business Connections',
+  description: 'Read spotlight articles about our community members. Learn about their businesses, achievements, and contributions to the network.',
+  ogTitle: 'Member Spotlights - Elite Business Connections',
+  ogDescription: 'Read spotlight articles about our community members. Learn about their businesses, achievements, and contributions to the network.',
+  ogImage: 'https://elitebusinessconnections.ca//images/ebc_logo_light.png',
+  ogUrl: 'https://elitebusinessconnections.ca/spotlights/',
+  twitterTitle: 'Member Spotlights - Elite Business Connections',
+  twitterDescription: 'Read spotlight articles about our community members. Learn about their businesses, achievements, and contributions to the network.',
+  twitterImage: 'https://elitebusinessconnections.ca//images/ebc_logo_light.png',
+  twitterCard: 'summary'
+})
+
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+
 const { formatDate } = useUtilities()
 
 const route = useRoute()
