b6f52bb9322a4bc79f50f499a7f21128c6c63ce1
daver987 <daveyalexander@outlook.com>
2023-10-23 07:58:12 -0400

    Add: query for testimonials

--- components/sections/Testimonials/Testimonial.vue
+++ components/sections/Testimonials/Testimonial.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup lang='ts'>
 import { Splide, SplideSlide } from '@splidejs/vue-splide'
 
 interface Testimonial {
@@ -9,11 +9,16 @@ interface Testimonial {
   role: string
 }
 
-interface Props {
-  testimonials: Testimonial[]
-}
 
-defineProps<Props>()
+const query = groq`*[_type == "testimonial"]{
+  name,
+  "avatar": avatar.asset->url,
+  company,
+    quote,
+    role
+}`
+
+const { data: testimonials } = await useSanityQuery(query)
 
 const options = {
   rewind: false,
@@ -34,44 +39,44 @@ const options = {
 </script>
 
 <template>
-  <section class="py-24 sm:py-32 bg-gray-900">
-    <div class="max-w-xl text-center mx-auto mb-24">
-      <h2 class="text-lg font-semibold leading-8 tracking-tight text-primary">
+  <section class='py-24 sm:py-32 bg-gray-900'>
+    <div class='max-w-xl text-center mx-auto mb-24'>
+      <h2 class='text-lg font-semibold leading-8 tracking-tight text-primary'>
         What Our Members Say
       </h2>
-      <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+      <p class='mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl'>
         Building Success, One Connection at a Time
       </p>
     </div>
     <UContainer>
       <Splide
-        class="max-w-7xl w-full"
-        :options="options"
-        aria-label="Testimonials"
+        class='max-w-7xl w-full'
+        :options='options'
+        aria-label='Testimonials'
       >
         <SplideSlide
-          class="flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 xl:pr-20 border-x-[0.5px] border-gray-700 pl-5"
-          v-for="testimonial in testimonials"
-          :key="testimonial.name"
+          class='flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 xl:pr-20 border-x-[0.5px] border-gray-700 pl-5'
+          v-for='testimonial in testimonials as Testimonial[]'
+          :key='testimonial.name'
         >
-          <h3 class="h-12 text-3xl font-medium text-gray-100 self-start">
+          <h3 class='h-12 text-3xl font-medium text-gray-100 self-start'>
             {{ testimonial.company }}
           </h3>
-          <figure class="mt-10 flex flex-auto flex-col justify-between">
-            <blockquote class="text-lg leading-8 text-white">
+          <figure class='mt-10 flex flex-auto flex-col justify-between'>
+            <blockquote class='text-lg leading-8 text-white'>
               <p>{{ testimonial.quote }}</p>
             </blockquote>
-            <figcaption class="mt-10 flex items-center gap-x-6">
+            <figcaption class='mt-10 flex items-center gap-x-6'>
               <NuxtImg
-                class="h-14 w-14 rounded-full bg-gray-800 object-center object-cover"
-                :src="testimonial.avatar"
-                alt="Avatar"
+                class='h-14 w-14 rounded-full bg-gray-800 object-center object-cover'
+                :src='testimonial.avatar'
+                alt='Avatar'
               />
-              <div class="text-base">
-                <div class="font-semibold text-white">
+              <div class='text-base'>
+                <div class='font-semibold text-white'>
                   {{ testimonial.name }}
                 </div>
-                <div class="mt-1 text-gray-400">
+                <div class='mt-1 text-gray-400'>
                   {{ testimonial.role }}
                 </div>
               </div>
--- pages/index.vue
+++ pages/index.vue
@@ -7,20 +7,10 @@ definePageMeta({
   colorMode: 'dark',
 })
 
-const query = groq`*[_type == "testimonial"]{
-  name,
-  "avatar": avatar.asset->url,
-  company,
-    quote,
-    role
-}`
-
-const { data: testimonialArray } = await useSanityQuery(query)
 const { data: teamMembers } = await useFetch('/api/team-members')
 
 const members = teamMembers.value?.members!
 const socialIcons = teamMembers.value?.socialIcons!
-const testimonials = testimonialArray.value
 </script>
 
 <template>
@@ -30,7 +20,7 @@ const testimonials = testimonialArray.value
     <Stats is-boxed />
     <MembersThreeUp :members="members as Member[]" :socialIcons="socialIcons" />
     <ClientOnly>
-      <Testimonial :testimonials="testimonials" />
+      <Testimonial />
     </ClientOnly>
     <CtaElevate />
   </div>
