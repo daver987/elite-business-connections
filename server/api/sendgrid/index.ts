8688926253486f98d9d986741bb826d62a871be2
daver987 <daveyalexander@outlook.com>
2023-11-04 15:19:23 -0400

    Added sendgrid integration to send emails using SendGrid API.

--- server/api/sendgrid/index.ts
+++ server/api/sendgrid/index.ts
@@ -4,17 +4,11 @@ const getConfig = () => {
   return useRuntimeConfig().SENDGRID_API_KEY
 }
 
-const msg = {
-  to: 'info@drobertson.pro',
-  from: 'contact@elitebusinessconnections.ca',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-const SENDGRID_TEMPLATE = "d-55354bbb9b7749d8b3b7938892330028"
 export default defineEventHandler(async (event) => {
+  const body = await readBody(event)
   const apiKey = getConfig()
   sgMail.setApiKey(apiKey)
+
 
   try {
     const response = await sgMail.send(msg)
