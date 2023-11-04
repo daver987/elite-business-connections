430069c07f47907223acc5d77507b45ea4818024
daver987 <daveyalexander@outlook.com>
2023-11-04 12:17:29 -0400

    aider: Refactored event handler to use async/await and return the response from SendGrid.
    # Aider chat conversation:
    
    USER:  please refactor this event handler to use async await and return the response from sendgrid
    
    ASSISTANT: To refactor the event handler to use async/await and return the response from SendGrid, we need to modify the `server/api/sendgrid/index.ts` file.
    
    Here are the changes:
    
    ```typescript
    server/api/sendgrid/index.ts
    <<<<<<< SEARCH
    export default defineEventHandler(async (event) => {
      const apiKey = getConfig()
      sgMail.setApiKey(apiKey)
    
      const response = await sgMail
        .send(msg)
        .then(() => {
          console.log('Email sent')
        })
        .catch((error) => {
          console.error(error)
        })
    
      return {response}
    })
    =======
    export default defineEventHandler(async (event) => {
      const apiKey = getConfig()
      sgMail.setApiKey(apiKey)
    
      try {
        const response = await sgMail.send(msg)
        console.log('Email sent')
        return {response}
      } catch (error) {
        console.error(error)
        return {error}
      }
    })
    >>>>>>> REPLACE
    ```
    
    In the `REPLACE` block, I've removed the `.then()` and `.catch()` methods and replaced them with a `try/catch` block. This is a more modern and readable way to handle promises and errors in JavaScript. The `await` keyword is used to pause the async function execution until the promise is resolved or rejected, then resumes the execution and returns the resolved value. If the promise is rejected, an error is thrown and caught in the `catch` block. The function then returns the response from SendGrid or the error.

--- server/api/sendgrid/index.ts
+++ server/api/sendgrid/index.ts
@@ -4,27 +4,24 @@ const getConfig = () => {
   return useRuntimeConfig().SENDGRID_API_KEY
 }
 
-
 const msg = {
   to: 'info@drobertson.pro',
   from: 'contact@elitebusinessconnections.ca',
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 }
 
 export default defineEventHandler(async (event) => {
   const apiKey = getConfig()
   sgMail.setApiKey(apiKey)
 
-  const response = await sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent')
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-
-  return {response}
+  try {
+    const response = await sgMail.send(msg)
+    console.log('Email sent')
+    return { response }
+  } catch (error) {
+    console.error(error)
+    return { error }
+  }
 })
