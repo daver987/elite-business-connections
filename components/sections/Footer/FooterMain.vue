a254233d9f4257b3c1f2fea75e4767992d7946db
daver987 <daveyalexander@outlook.com>
2023-11-04 22:19:42 -0400

    Add logic to Footer for the subscription to the newsletter. Tested OK

--- components/sections/Footer/FooterMain.vue
+++ components/sections/Footer/FooterMain.vue
@@ -1,18 +1,20 @@
-<script setup lang='ts'>
+<script setup lang="ts">
 import { ref } from '#imports'
-import type { FormSubmitEvent, NotificationColor } from '@nuxt/ui/dist/runtime/types'
+import type {
+  FormSubmitEvent,
+  NotificationColor,
+} from '@nuxt/ui/dist/runtime/types'
 import { subscriptionSchema } from '~/types/Subscription'
 import type { FooterNavigation } from '~/types/Navigation'
-import { useToast } from '@nuxt/ui/dist/runtime/composables'
 
-const {
-  data: navigation
-} = useFetch<FooterNavigation>('/api/navigation?navType=footer')
+const { data: navigation } = useFetch<FooterNavigation>(
+  '/api/navigation?navType=footer'
+)
 
 const currentYear = new Date().getFullYear()
 
 const state = reactive({
-  email_address: undefined
+  email_address: undefined,
 })
 
 const loading = ref(false)
@@ -31,7 +33,7 @@ async function showToast(
     title: title,
     description: description,
     timeout: 7000,
-    icon: icon
+    icon: icon,
   })
 }
 
@@ -45,9 +47,9 @@ type SendgridResponse = {
 
 async function submit(event: FormSubmitEvent<{ email: string }>) {
   loading.value = true
-  const response = await $fetch<SendgridResponse>('/api/sendgrid', {
+  const response = await $fetch<SendgridResponse>('/api/subscribe', {
     method: 'POST',
-    body: event.data
+    body: event.data,
   })
 
   if (response.statusCode === 202) {
@@ -75,108 +77,110 @@ async function submit(event: FormSubmitEvent<{ email: string }>) {
 </script>
 
 <template>
-  <footer class='bg-gray-900' aria-labelledby='footer-heading'>
-    <h2 class='sr-only' id='footer-heading'>Footer</h2>
-    <UContainer class='pb-8 pt-20 sm:pt-24 lg:pt-32'>
-      <div class='xl:grid xl:grid-cols-3 xl:gap-8'>
-        <div class='grid grid-cols-2 gap-8 xl:col-span-2'>
-          <div class='md:grid md:grid-cols-2 md:gap-8'>
+  <footer class="bg-gray-900" aria-labelledby="footer-heading">
+    <h2 class="sr-only" id="footer-heading">Footer</h2>
+    <UContainer class="pb-8 pt-20 sm:pt-24 lg:pt-32">
+      <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+        <div class="grid grid-cols-2 gap-8 xl:col-span-2">
+          <div class="md:grid md:grid-cols-2 md:gap-8">
             <div>
-              <h3 class='text-base font-semibold leading-6 text-white'>
+              <h3 class="text-base font-semibold leading-6 text-white">
                 Opportunities
               </h3>
-              <ul class='mt-6 space-y-4' role='list'>
-                <li v-for='item in navigation?.opportunities' :key='item.name'>
+              <ul class="mt-6 space-y-4" role="list">
+                <li v-for="item in navigation?.opportunities" :key="item.name">
                   <NuxtLink
-                    class='text-sm leading-6 text-gray-300 hover:text-white'
-                    :href='item.href'
-                  >{{ item.name }}
+                    class="text-sm leading-6 text-gray-300 hover:text-white"
+                    :href="item.href"
+                    >{{ item.name }}
                   </NuxtLink>
                 </li>
               </ul>
             </div>
-            <div class='mt-10 md:mt-0'>
-              <h3 class='text-base font-semibold leading-6 text-white'>
+            <div class="mt-10 md:mt-0">
+              <h3 class="text-base font-semibold leading-6 text-white">
                 About
               </h3>
-              <ul class='mt-6 space-y-4' role='list'>
-                <li v-for='item in navigation?.about' :key='item.name'>
+              <ul class="mt-6 space-y-4" role="list">
+                <li v-for="item in navigation?.about" :key="item.name">
                   <NuxtLink
-                    class='text-sm leading-6 text-gray-300 hover:text-white'
-                    :to='item.href'
-                  >{{ item.name }}
+                    class="text-sm leading-6 text-gray-300 hover:text-white"
+                    :to="item.href"
+                    >{{ item.name }}
                   </NuxtLink>
                 </li>
               </ul>
             </div>
           </div>
-          <div class='md:grid md:grid-cols-2 md:gap-8'>
+          <div class="md:grid md:grid-cols-2 md:gap-8">
             <div>
-              <h3 class='text-base font-semibold leading-6 text-white'>
+              <h3 class="text-base font-semibold leading-6 text-white">
                 Resources
               </h3>
-              <ul class='mt-6 space-y-4' role='list'>
-                <li v-for='item in navigation?.resources' :key='item.name'>
+              <ul class="mt-6 space-y-4" role="list">
+                <li v-for="item in navigation?.resources" :key="item.name">
                   <NuxtLink
-                    class='text-sm leading-6 text-gray-300 hover:text-white'
-                    :to='item.href'
-                  >{{ item.name }}
+                    class="text-sm leading-6 text-gray-300 hover:text-white"
+                    :to="item.href"
+                    >{{ item.name }}
                   </NuxtLink>
                 </li>
               </ul>
             </div>
-            <div class='mt-10 md:mt-0'>
-              <h3 class='text-base font-semibold leading-6 text-white'>
+            <div class="mt-10 md:mt-0">
+              <h3 class="text-base font-semibold leading-6 text-white">
                 Legal
               </h3>
-              <ul class='mt-6 space-y-4' role='list'>
-                <li v-for='item in navigation?.legal' :key='item.name'>
+              <ul class="mt-6 space-y-4" role="list">
+                <li v-for="item in navigation?.legal" :key="item.name">
                   <NuxtLink
-                    class='text-sm leading-6 text-gray-300 hover:text-white'
-                    :to='item.href'
-                  >{{ item.name }}
+                    class="text-sm leading-6 text-gray-300 hover:text-white"
+                    :to="item.href"
+                    >{{ item.name }}
                   </NuxtLink>
                 </li>
               </ul>
             </div>
           </div>
         </div>
-        <div class='mt-10 xl:mt-0'>
-          <h3 class='text-base font-semibold leading-6 text-white'>
+        <div class="mt-10 xl:mt-0">
+          <h3 class="text-base font-semibold leading-6 text-white">
             Subscribe to our newsletter
           </h3>
-          <p class='my-2 text-sm leading-6 text-gray-300'>
+          <p class="my-2 text-sm leading-6 text-gray-300">
             The latest news, articles, and resources, sent to your inbox weekly.
           </p>
 
-          <UForm :schema='subscriptionSchema' :state='state' @submit='submit'>
-            <UFormGroup label='Email address' name='email' required>
+          <UForm :schema="subscriptionSchema" :state="state" @submit="submit">
+            <UFormGroup label="Email address" name="email" required>
               <UInput
-                v-model='state.email_address'
-                placeholder='Enter your email'
-                size='lg'
-                type='email'
+                v-model="state.email_address"
+                placeholder="Enter your email"
+                size="lg"
+                type="email"
               />
             </UFormGroup>
-            <UButton size='lg' block type='submit' :loading='loading'>Subscribe</UButton>
+            <UButton size="lg" block type="submit" :loading="loading"
+              >Subscribe</UButton
+            >
           </UForm>
         </div>
       </div>
       <div
-        class='mt-16 border-t border-white/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24'
+        class="mt-16 border-t border-white/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24"
       >
-        <div class='flex space-x-6 md:order-2'>
+        <div class="flex space-x-6 md:order-2">
           <NuxtLink
-            class='text-gray-500 hover:text-gray-400'
-            v-for='item in navigation?.social'
-            :key='item.name'
-            :to='item.href'
+            class="text-gray-500 hover:text-gray-400"
+            v-for="item in navigation?.social"
+            :key="item.name"
+            :to="item.href"
           >
-            <span class='sr-only'>{{ item.name }}</span>
-            <Icon class='w-6 h-6' :name='item.icon' aria-hidden='true' />
+            <span class="sr-only">{{ item.name }}</span>
+            <Icon class="w-6 h-6" :name="item.icon" aria-hidden="true" />
           </NuxtLink>
         </div>
-        <p class='mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0'>
+        <p class="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
           &copy; {{ currentYear }} Elite Business Connections All rights
           reserved.
         </p>
